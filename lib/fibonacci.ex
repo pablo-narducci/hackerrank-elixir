90604d9cd864ae561c7ade450094cf83a5ee5bbe
Pablo Narducci <pnarducci@cosmolex.com>
2021-05-31 19:20:28 -0300

    Fibonacci

---
+++ lib/fibonacci.ex
@@ -0,0 +1,15 @@
+defmodule Fibonacci do
+  def main do
+    [a] = IO.read(:stdio, :line)
+    |> String.split([" ", "\n"])
+    |> Enum.filter(fn x -> String.length(x) > 0 end)
+    |> Enum.map(&String.to_integer(&1))
+
+    fibonacci(a)
+    |> IO.puts
+  end
+
+  def fibonacci(n) when n == 1,  do: 0
+  def fibonacci(n) when n == 2,  do: 1
+  def fibonacci(n), do: fibonacci(n-1) + fibonacci(n-2)
+end
