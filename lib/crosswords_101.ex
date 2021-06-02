fbc1a88df332f21f7ada6654109ccfee8b8ca6c2
Pablo Narducci <pnarducci@cosmolex.com>
2021-06-02 19:28:40 -0300

    Starting Crosswords 101

--- lib/crosswords_101.ex
+++ lib/crosswords_101.ex
@@ -3,6 +3,24 @@ defmodule Crosswords101 do
   https://www.hackerrank.com/challenges/crosswords-101/problem
   [ ] Solved
   """
+  # Word representation:
+  # %{32 => 'A', 33 => 'B', 34 => 'C' }
+  # %{32 => '-', 33 => 'B', 34 => '-' }
 
+  # grid List<Map<int, char>>
+  # Is consistent?(grid): Map.merge with no conflicts
+
+  # fit word_template: Map<int, char>, words: List<string> -> grid List<Map<int, char>>
+
+  # fits? word_template Map<int, char>, word : string -> bool
+
+  def fits?(word_template, word) when map_size(word_template) != length(word) do
+    false
+  end
+
+  def fits?(word_template, word) do
+    IO.puts("#{map_size(word_template)} == #{length(word)}")
+    true
+  end
 
 end
--- test/hackerrank_test.exs
+++ test/hackerrank_test.exs
@@ -1,8 +1,17 @@
 defmodule HackerrankTest do
   use ExUnit.Case
-  doctest Hackerrank
+  doctest Crosswords101
 
-  test "greets the world" do
-    assert Hackerrank.hello() == :world
+  test "London should fit vertically" do
+    word_template = %{
+      0 => '-',
+      1 => 'O',
+      2 => '-',
+      3 => '-',
+      4 => '-',
+      5 => '-'
+    }
+
+    assert Crosswords101.fits?(word_template, to_char_list("LONDON")) == true
   end
 end
